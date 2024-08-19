1a97f0a17dd8e9f42fbd15f5ab700f53482e8094
VitorLima1998 <vitor.xlr8@gmail.com>
2024-08-19 15:37:10 -0400

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./_components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Home
