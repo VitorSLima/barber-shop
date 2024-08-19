f5ad476635aab31e2c0c0e8aa910d4829d27764a
VitorLima1998 <vitor.xlr8@gmail.com>
2024-08-19 16:01:52 -0400

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,97 @@
-import { Button } from "./_components/ui/button"
+import Image from "next/image"
+import Header from "@/app/_components/header"
+import BarberShopItem from "@/app/_components/barbershop-item"
+import { SearchIcon } from "lucide-react"
+import { Button } from "@/app/_components/ui/button"
+import { Input } from "@/app/_components/ui/input"
+import { Card, CardContent } from "@/app/_components/ui/card"
+import { db } from "@/app/_lib/prisma"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
-const Home = () => {
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+  const popularBarberShops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
   return (
     <div>
-      <h1>Home</h1>
-      <Button>Click me</Button>
+      {/* HEADER */}
+      <Header />
+      {/* TEXTO */}
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Vitor!</h2>
+        <p>Domingo, 18 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* BUSCA RÁPIDO  */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button key={option.title} className="gap-2" variant="secondary">
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com a Barber Shop"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTOS */}
+        <BookingItem />
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Popular
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarberShops.map((barbershop) => (
+            <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              © 2024 Copyright <span className="font-bold">Barber Shop</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
