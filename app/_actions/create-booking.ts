23f9fad780efe072958d2f3983b387749bd7d66f
VitorSLima <dev.vitorlima@gmail.com>
2024-08-31 15:25:45 -0400

    feat: ensure only authenticated user can book

--- app/_actions/create-booking.ts
+++ app/_actions/create-booking.ts
@@ -2,16 +2,21 @@
 
 import { revalidatePath } from "next/cache"
 import { db } from "../_lib/prisma"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../_lib/auth"
 
 interface CreateBookingProps {
   serviceId: string
-  userId: string
   date: Date
 }
 
 export const createBooking = async (params: CreateBookingProps) => {
+  const user = await getServerSession(authOptions)
+  if (!user) {
+    throw new Error("Usuário não autenticado")
+  }
   await db.booking.create({
-    data: params,
+    data: { ...params, userId: (user.user as any).id },
   })
 
   revalidatePath("barbershops/[id]")
--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -117,7 +117,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
       })
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
       handleBookingSheetOpenChange()
