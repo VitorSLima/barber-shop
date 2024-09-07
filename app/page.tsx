3797f3d24f712e751a186ef38e909b53a1a75f7d
VitorSLima <dev.vitorlima@gmail.com>
2024-09-07 15:29:46 -0400

    feat: update welcome mesage

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,19 @@ const Home = async () => {
       <Header />
       {/* TEXTO */}
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Vitor!</h2>
-        <p>Domingo, 18 de agosto.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session?.user?.name : "Bem vindo!"}
+        </h2>
+
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span> de </span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         <div className="mt-6">
           <Search />
