a37eb6bc3204a17dcf38c3dd32ed4a640db666b1
VitorSLima <dev.vitorlima@gmail.com>
2024-09-07 17:30:35 -0400

    refactor: update copyright owner in footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <Card>
         <CardContent className="px-5 py-6">
           <p className="text-sm text-gray-400">
-            © 2024 Copyright <span className="font-bold">Barber Shop</span>
+            © 2024 Copyright <span className="font-bold">Vitor Lima</span>
           </p>
         </CardContent>
       </Card>
