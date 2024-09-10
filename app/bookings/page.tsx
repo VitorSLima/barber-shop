954cff59e752c819cf4cb66ccafda42b5158230d
VitorSLima <dev.vitorlima@gmail.com>
2024-09-09 20:12:29 -0400

    refactor: ensure decimal error is not shown on console

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -57,7 +57,10 @@ const Bookings = async () => {
               Confirmados
             </h2>
             {confirmedBookings.map((booking) => (
-              <BookingItem key={booking.id} booking={booking} />
+              <BookingItem
+                key={booking.id}
+                booking={JSON.parse(JSON.stringify(booking))}
+              />
             ))}
           </>
         )}
@@ -67,7 +70,10 @@ const Bookings = async () => {
               Finalizados
             </h2>
             {concluedBookings.map((booking) => (
-              <BookingItem key={booking.id} booking={booking} />
+              <BookingItem
+                key={booking.id}
+                booking={JSON.parse(JSON.stringify(booking))}
+              />
             ))}
           </>
         )}
