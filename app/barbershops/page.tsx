1825af287862f659dc39b60343e9f360e7a54344
VitorSLima <dev.vitorlima@gmail.com>
2024-09-16 10:57:49 -0400

    refactor: Optimize data fetching in barbershop page component

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -1,42 +1,13 @@
-import { db } from "../_lib/prisma"
 import Header from "../_components/header"
 import Search from "../_components/search"
 import BarberShopItem from "../_components/barbershop-item"
-
-interface BarbershopsPageProps {
-  searchParams: {
-    title?: string
-    service?: string
-  }
-}
+import {
+  BarbershopsPageProps,
+  getBarbershopsBySearchParams,
+} from "../_data/get-barbershops-by-searchparams"
 
 const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
-  const barbershops = await db.barbershop.findMany({
-    where: {
-      OR: [
-        searchParams?.title
-          ? {
-              name: {
-                contains: searchParams?.title,
-                mode: "insensitive",
-              },
-            }
-          : {},
-        searchParams?.service
-          ? {
-              services: {
-                some: {
-                  name: {
-                    contains: searchParams?.service,
-                    mode: "insensitive",
-                  },
-                },
-              },
-            }
-          : {},
-      ],
-    },
-  })
+  const barbershops = await getBarbershopsBySearchParams({ searchParams })
 
   return (
     <div>
@@ -46,7 +17,8 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams?.title || searchParams?.service}&quot;
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
         </h2>
 
         <div className="grid grid-cols-2 gap-4">
